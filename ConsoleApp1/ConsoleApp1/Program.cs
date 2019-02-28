2ed2ef144cb2895914cd4d1da6a7fe65a976ffe8
Timothy Hansen <30021573@student.toiohomai.ac.nz>
2019-02-28 13:27:42 +1300

    thanks

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter first whole number");
+            int Fnum = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter Second whole number");
+            int Snum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter double number");
+            double dec1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter double number");
+            double dec2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("how was your day");
+            Console.ReadLine();
+
+            Console.WriteLine("I hope you have a great rest of your day");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
